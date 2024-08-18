36bceea83ba41c866c8359d66ccff192560d9077
H-Nika <88vr07@gmail.com>
2024-08-18 23:38:01 +0300

    updFavorite

--- src/pages/favorites-page/FavoritesPage.jsx
+++ src/pages/favorites-page/FavoritesPage.jsx
@@ -1,28 +1,17 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import DocumentTitle from '../../components/document-title/DocumentTitle';
-// import { fetchContacts } from '../../redux/adverts/operations';
-// import { selectContactsLoading } from '../../redux/adverts/selectors';
-// import ContactForm from '../../components/contact-form/ContactForm';
-// import ContactList from '../../components/contact-list/ContactList';
-// import SearchBox from '../../components/search-box/SearchBox';
+import css from './FavoritesPage.module.css';
 
 const FavoritesPage = () => {
-  // const dispatch = useDispatch();
-  // const isLoading = useSelector(selectContactsLoading);
 
-  // useEffect(() => {
-  //   dispatch(fetchContacts());
-  // }, [dispatch]);
 
   return (
     <>
       <DocumentTitle>Favorites</DocumentTitle>
-      <div>FavoritesPage</div>
-      {/* <div>{isLoading && 'Request in progress...'}</div>
-      <ContactForm />
-      <SearchBox />
-      <ContactList /> */}
+      <div className={css.container}>
+      <div className={css.title}>FavoritesPage</div>
+        <span className={css.span}>This page is being created to display your preferences. We are doing everything we can to get it up and running as soon as possible!</span>
+        <span className={css.span1}>Thank you for your understanding!</span>
+      </div>
     </>
   );
 };
